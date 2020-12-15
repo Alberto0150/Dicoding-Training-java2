2a7ec96083800bae89db54958eb0fc091df56f7e
Alberto0150 <albertosanjaya2@gmail.com>
2020-12-15 13:36:23 +0700

    Codelab BufferedReader

---
+++ src/com/dicoding/javafundamental/inputoutput/UserBufferedReader.java
@@ -0,0 +1,29 @@
+package com.dicoding.javafundamental.inputoutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserBufferedReader {
+    public static void main (String[] args)
+    {
+        InputStreamReader streamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+        System.out.println("program penjumlahan");
+        int value = 0 ;
+        int anothervalue = 0;
+        try{
+            System.out.print("masukan angka pertama :");
+            value = Integer.parseInt(bufferedReader.readLine());
+            System.out.print("masukkan angka kedua :");
+            anothervalue = Integer.parseInt(bufferedReader.readLine());
+        }
+        catch(IOException e) {
+        e.printStackTrace();
+        }
+
+        int result = value + anothervalue ;
+        System.out.println("Hasilnya : " + result ) ;9
+    }
+
+}
